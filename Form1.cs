30279b332e5269d217eb539507bf974fafc34f69
oguzhantavsan <48738811+oguzhantavsan@users.noreply.github.com>
2019-11-09 22:55:54 +0300

    add client code from lab exercise and add server code from lab

--- Form1.cs
+++ Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,84 @@ namespace Cs408Project
 {
     public partial class Form1 : Form
     {
+        bool terminating = false;
+        bool connected = false;
+        Socket clientSocket;
         public Form1()
         {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
             InitializeComponent();
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string IP = textBox_ip.Text;
+            int portNum;
+            if (Int32.TryParse(textBox_port.Text, out portNum))
+            {
+                try
+                {
+                    clientSocket.Connect(IP, portNum);
+                    button_connect.Enabled = false;
+                    connected = true;
+                    logs.AppendText("Connected established...\n");
+                    logs.AppendText("Your message has been sent!\n");
+                    string message = textBox_name.Text;
+                    Thread receiveThread = new Thread(Receive);
+                    receiveThread.Start();
+                    if (message != "" && message.Length <= 64)
+                    {
+                        Byte[] buffer = new Byte[64];
+                        buffer = Encoding.Default.GetBytes(message);
+                        clientSocket.Send(buffer);
+                    }
+                }
+                catch
+                {
+                    logs.AppendText("Could not connect to the server!\n");
+                }
+            }
+            else
+            {
+                logs.AppendText("Check the port\n");
+            }
+
+        }
+        private void Receive()
+        {
+            while (connected)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    clientSocket.Receive(buffer);
+
+                    string incomingMessage = Encoding.Default.GetString(buffer);
+                    incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+                    if (incomingMessage != "")
+                        logs.AppendText("Server: " + incomingMessage + "\n");
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        logs.AppendText("The server has disconnected\n");
+                        button_connect.Enabled = true;
+                    }
+
+                    clientSocket.Close();
+                    connected = false;
+                }
+
+            }
+        }
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            connected = false;
+            terminating = true;
+            Environment.Exit(0);
         }
     }
 }
